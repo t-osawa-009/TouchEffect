1fc4f93c3aca20c506f34f8b021f09ffa7ef54e0
t-osawa-009 <da87435@gmail.com>
2021-09-07 21:08:41 +0900

    Update Demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -4,9 +4,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        let ac = UIAlertController(title: "Tap!", message: nil, preferredStyle: .alert)
+        ac.addAction(.init(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
 }
 
