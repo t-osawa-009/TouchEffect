84b5994dedb5ec16ca1d3d2390a168a2a602ad2c
t-osawa-009 <da87435@gmail.com>
2021-09-07 20:38:12 +0900

    Modify effect UI

--- TouchEffect/TouchEventController.swift
+++ TouchEffect/TouchEventController.swift
@@ -4,10 +4,12 @@ import UIKit
 public class TouchEventController {
     public struct Config {
         public var isActive = true
-        public var color: UIColor = Color.pink
+        public var color: UIColor = Color.blue
+        public var duration: TimeInterval = 0.1
         public struct Color {
             public static let blue: UIColor = .init(red: 144 / 255, green: 215 / 255, blue: 236 / 255, alpha: 0.8)
             public static let pink: UIColor = .init(red: 255 / 255, green: 182 / 255, blue: 193 / 255, alpha: 0.8)
+            public static let black: UIColor = .init(red: 0, green: 0, blue: 0, alpha: 0.8)
         }
     }
     static let shared = TouchEventController()
@@ -28,15 +30,17 @@ public class TouchEventController {
             switch touch.phase {
             case .began:
                 let view = TouchView()
-                view.backgroundColor = config.color
+                view.backgroundColor = config.color.withAlphaComponent(0.3)
+                view.layer.borderWidth = 3.0
+                view.layer.borderColor = config.color.cgColor
                 view.touch = touch
                 view.center = touch.location(in: topWindow)
                 topWindow.addSubview(view)
                 touchViews.insert(view)
                 
                 view.alpha = 0
-                view.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
-                let animator = UIViewPropertyAnimator(duration: 0.2, curve: .easeIn) {
+                view.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+                let animator = UIViewPropertyAnimator(duration: config.duration, curve: .easeIn) {
                     view.transform = .identity
                     view.alpha = 1.0
                 }
@@ -47,7 +51,7 @@ public class TouchEventController {
                 }
             case .ended, .cancelled:
                 if let view = findTouchView(touch) {
-                    let animator = UIViewPropertyAnimator(duration: 0.2, curve: .easeIn) {
+                    let animator = UIViewPropertyAnimator(duration: config.duration, curve: .easeIn) {
                         view.alpha = 0.0
                     }
                     animator.addCompletion { [weak self] _ in
